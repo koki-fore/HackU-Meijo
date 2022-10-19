97c2c29bab991b8619787a4143b4e73feb6b4aed
koki-fore <furukunn@gmail.com>
2022-10-19 23:47:10 +0900

    passwordを入れるところ作ってみた

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -4,11 +4,13 @@ import App from './App'
 import './index.css'
 
 import { ChakraProvider } from "@chakra-ui/react"
+import PasswordInput from './components/PasswordInput'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
+      <PasswordInput />
     </ChakraProvider>
   </React.StrictMode>
 )
